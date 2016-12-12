4192c33f842cbbe5b1c5c1ed7f2a4c6b7b3d2bb7
Amy Chan <chamy09@gmail.com>
2016-12-12 16:10:25 -0600

    initial commit

---
+++ Assets/AnalyticsScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnalyticsScript : MonoBehaviour {
+
+	public GoogleAnalyticsV4 googleAnalytics;
+
+	// Use this for initialization
+	void Start () {
+		googleAnalytics.LogScreen ("Main");
+		googleAnalytics.LogScreen(new AppViewHitBuilder().SetScreenName("Main"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
